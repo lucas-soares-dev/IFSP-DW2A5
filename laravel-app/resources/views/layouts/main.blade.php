270c6fb4638879cb7b821cedfcb813aece018d18
Lucas Soares <lucas@omny.com.br>
2022-05-25 22:04:22 -0300

    flash messages

--- laravel-app/resources/views/layouts/main.blade.php
+++ laravel-app/resources/views/layouts/main.blade.php
@@ -39,8 +39,18 @@
             </nav>
         </header>
 
-        @yield('content')
-        
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if (session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+
+                    @yield('content')
+                </div>
+            </div>
+        </main>
+
         <footer>
             <p>&copy; 2022 Todos os direitos reservados</p>
         </footer>
