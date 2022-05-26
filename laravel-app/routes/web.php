931da1952a113ab82758bf31eab5e51a391ee6e0
Lucas Soares <lucas@omny.com.br>
2022-05-25 21:57:05 -0300

    salvar dados no banco de dados

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'EventController@index');
 Route::get('/events/create', 'EventController@create');
+Route::post('/events', 'EventController@store');
